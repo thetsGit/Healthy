f2945b266385c49a1f5bb438e9e65a681545d523
Thethan <eagleking495@gmail.com>
2022-03-02 22:40:45 +0630

    init: Initiate required assets

---
+++ components/styled/NavBarStyled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const NavBarStyled = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export default NavBarStyled;
