eb3de460a9686080a203f763c4bb8c364d08b876
thethan <thethan.code@gmail.com>
2022-03-11 13:14:33 +0630

    refactor: Refactor footer section part 2

--- components/home/Footer.js
+++ components/home/Footer.js
@@ -5,6 +5,7 @@ import Rectangle from "../styled/Rectangle"
 import contactData from './footer/data/contact';
 import termsData from './footer/data/terms';
 import ListLinks from './footer/ListLinks';
+import SocialLinks from './footer/SocialLinks';
 
 export default function Footer() {
   return (
@@ -13,22 +14,7 @@ export default function Footer() {
             <img src='images/logo.svg' className='logo' alt="logo svg" />
             <ListLinks data={contactData} />
             <ListLinks data={termsData} />
-            <div className='social-links'>
-                <ul>
-                    <li><a href="https://www.linkedin.com/in/thet-lin-han-0a5693227/"><i class="fa-brands fa-linkedin"></i></a></li>
-                    <li><a href="https://www.facebook.com/thethan13/"><i class="fa-brands fa-facebook"></i></a></li>
-                    <li><a href="https://github.com/thetsGit"><i class="fa-brands fa-github"></i></a></li>
-                </ul>
-                <p>
-                <p>
-                    <i>
-                    <span><i class="fa-solid fa-less-than"></i> / <i class="fa-solid fa-greater-than"></i> by <em><a href='https://www.linkedin.com/in/thet-lin-han-0a5693227/' className='me'>Thethan</a></em></span>
-                    <br/>
-                    <span>&copy; Mao</span>     
-                    </i>
-                </p>
-                </p>
-            </div>
+            <SocialLinks />
         </FooterStyled>
         <Rectangle className='rectangle' />
     </ContainerStyled>
---
+++ components/home/footer/SocialLinks.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function SocialLinks() {
+  return (
+    <div className='social-links'>
+        <ul>
+            <li><a href="https://www.linkedin.com/in/thet-lin-han-0a5693227/"><i class="fa-brands fa-linkedin"></i></a></li>
+            <li><a href="https://www.facebook.com/thethan13/"><i class="fa-brands fa-facebook"></i></a></li>
+            <li><a href="https://github.com/thetsGit"><i class="fa-brands fa-github"></i></a></li>
+        </ul>
+        <p>
+        <p>
+            <i>
+            <span><i class="fa-solid fa-less-than"></i> / <i class="fa-solid fa-greater-than"></i> by <em><a href='https://www.linkedin.com/in/thet-lin-han-0a5693227/' className='me'>Thethan</a></em></span>
+            <br/>
+            <span>&copy; Mao</span>     
+            </i>
+        </p>
+        </p>
+    </div>
+  )
+}
