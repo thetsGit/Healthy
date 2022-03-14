7f0b3dc90652eb694b7ab8fde5df6973d85f2242
thethan <thethan.code@gmail.com>
2022-03-14 18:44:43 +0630

    feat: add display name to hero section action component

--- components/home/hero/ContentBox/Action.js
+++ components/home/hero/ContentBox/Action.js
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import React, { forwardRef } from "react";
 import ButtonStyled from "@/styled/hero/ButtonStyled";
 
-const Action = forwardRef(function (props, ref) {
+const Action = forwardRef(function Action (props, ref) {
   return (
     <div className="action-btns" ref={ref}>
       <ButtonStyled className="button">
