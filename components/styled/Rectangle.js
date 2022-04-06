465c9ec5cf757e9660c724c0002561760fe5cd89
thethan <thethan.code@gmail.com>
2022-04-06 10:32:09 +0630

    fix: Change bg to original one

--- components/styled/Rectangle.js
+++ components/styled/Rectangle.js
@@ -4,10 +4,10 @@ const Rectangle = styled.div`
   border-radius: 2rem;
   background: linear-gradient(
     45deg,
-    #fff,
+    #f8f8f8,
     ${({ theme: { colors } }) => colors.background}
   );
-  box-shadow: ${({ theme: { shadow } }) => shadow.sm};
+  box-shadow: ${({ theme: { shadow } }) => shadow.md};
   width: 15rem;
   height: 15rem;
   position: absolute;
